2707148759623431f9fc00bf7fc292e6a777e2ac
Naninidemo <trumpet.2100@gmail.com>
2024-01-05 21:42:34 +0900

    222725 down追加

--- database/migrations/2023_12_07_220023_add_column_to_users_table.php
+++ database/migrations/2023_12_07_220023_add_column_to_users_table.php
@@ -24,7 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            
+            $table->dropColumn('employee_number');
+            $table->dropColumn('job_title');
         });
     }
 };
